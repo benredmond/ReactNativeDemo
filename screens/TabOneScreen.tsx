c889eca3432c72586bee09bcc3848ac342bdf431
benredmond <bjr0206@gmail.com>
2021-02-28 17:50:06 -0600

    inital commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,45 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import {StyleSheet, Platform, Button} from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import {Text, View} from '../components/Themed';
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+export default function TabOneScreen({navigation, route}) {
+    const { data } = route.params;
+
+    return (
+        // View = div
+        // Text = p
+        // Many other components at https://reactnative.dev/docs/components-and-apis
+        <View style={styles.container}>
+            <Text style={styles.title}>Tab One</Text>
+            {Platform.select({
+                ios: <Text>on ios</Text>,
+                android: <Text>on android</Text>,
+                web: <Text>on web</Text>
+            })}
+            <Text>{data}</Text>
+            <Button title="Go to tab 2!" onPress={() => navigation.navigate("Root",
+                {screen: 'TabTwo',
+                params: { screen:'TabTwoScreen',
+                    params: {moreData: 'jane' }}})}/>
+            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    },
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,32 +1,30 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import {Platform, StyleSheet, Button} from 'react-native';
+import styled from 'styled-components/native'
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import {Text, View} from '../components/Themed';
 
-export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+const StyledView = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+
+const StyledText = styled.Text`
+  font-size: 20;
+  font-weight: bold;
+`
+
+export default function TabTwoScreen({route}) {
+    let moreData = route.params ? route.params.moreData : "";
+    return (
+        <StyledView>
+            <StyledText>Tab Two</StyledText>
+            {Platform.select({
+                android: <Text>android exclusive</Text>,
+            })}
+            <Text>{moreData}</Text>
+        </StyledView>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
